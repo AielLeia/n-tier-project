8b749b420fe2845cd0f29215e475d111c47414c6
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2021-12-10 08:23:04 +0100

    initialisation du projet

---
+++ back/src/test/java/fr/mai/ntiers/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mai.ntiers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
